944fb489ef3071fa308ea83f263dabffeb9cb012
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-08-06 15:06:50 +0530

    changes

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const books = [{
+        bookName: "Rudest Book Ever",
+        bookAuthor: "Shwetabh Gangwar",
+        bookPages: 200,
+        bookPrice: 240,
+        bookState: "Available"
+    },
+    {
+        bookName: "Do Epic Shit",
+        bookAuthor: "Ankur Wariko",
+        bookPages: 200,
+        bookPrice: 240,
+        bookState: "Available"
+    }
+]
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.get("/", function (req, res) {
+    res.render("home", {
+        data: books
+    })
+})
+
+app.listen(3000, (req, res) => {
+    console.log("App is running on port 3000")
+})
